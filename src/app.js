dfc45a0c917244395af49274875119f33c7b77a8
Philipp Burgmer <burgmer@w11k.de>
2016-03-11 11:29:00 +0100

    add first version

--- src/app.js
+++ src/app.js
@@ -5,9 +5,72 @@
   function configureSlides(slidesConfig) {
     slidesConfig.slides = [
       'title',
-      'about-me',
-      'about-us',
 
+      'intro/about-me',
+      'intro/about-us',
+      'intro/me-and-security',
+
+      'intro/aim',
+      'intro/breach-origin',
+      'intro/technologies',
+      'intro/understand',
+      'intro/we-and-security',
+      'intro/getting-information',
+      'intro/approaches',
+
+      'blackbox-testing/title',
+      'blackbox-testing/overview',
+      'blackbox-testing/encoding',
+      'blackbox-testing/kali-linux',
+      'blackbox-testing/proxy',
+      'blackbox-testing/zap-firefox',
+      'blackbox-testing/telnet',
+      'blackbox-testing/hands-on',
+
+      'blackbox-testing/analyse/title',
+      'blackbox-testing/analyse/overview',
+      'blackbox-testing/analyse/technologies',
+      'blackbox-testing/analyse/application',
+      'blackbox-testing/analyse/input',
+      'blackbox-testing/analyse/files-folders',
+      'blackbox-testing/analyse/automate',
+      'blackbox-testing/analyse/whole-purpose',
+      'blackbox-testing/analyse/hands-on',
+      'blackbox-testing/analyse/for-developers',
+
+      'blackbox-testing/access-handling/title',
+      'blackbox-testing/access-handling/overview',
+      'blackbox-testing/access-handling/auth-procedure',
+      'blackbox-testing/access-handling/auth-hands-on',
+      'blackbox-testing/access-handling/session-procedure',
+      'blackbox-testing/access-handling/session-hands-on',
+      'blackbox-testing/access-handling/access-procedure',
+      'blackbox-testing/access-handling/access-hands-on',
+      'blackbox-testing/access-handling/automate',
+      'blackbox-testing/access-handling/for-developers',
+
+      'blackbox-testing/input-handling/title',
+      'blackbox-testing/input-handling/overview',
+      'blackbox-testing/input-handling/attacks',
+      'blackbox-testing/input-handling/automate',
+      'blackbox-testing/input-handling/hands-on',
+      'blackbox-testing/input-handling/for-developers',
+
+      //'blackbox-testing/logic-flaws/title',
+      //'blackbox-testing/logic-flaws/hands-on',
+
+      'code-review/title',
+      'code-review/overview',
+      'code-review/advantages',
+      'code-review/interesting-parts',
+      'code-review/tools',
+      'code-review/example-xss',
+      'code-review/example-sql-injection',
+      'code-review/example-path-traversal',
+      'code-review/example-comments',
+      //'code-review/hands-on',
+
+      'summary',
       'end'
     ];
 
